edf73cceea9ddc07c4777ed4ae2ece22fa80cc95
Artur <Artur@192.168.1.15>
2014-01-05 23:37:48 +0100

    There was some bugs.

---
+++ src/po2/GameThreat.java
@@ -0,0 +1,12 @@
+package po2;
+
+/**
+ *
+ * @author Artur
+ */
+public class GameThreat implements Runnable {
+    @Override
+    public void run(){
+        System.out.println("Game");
+    }
+}
--- src/po2/PO2.java
+++ src/po2/PO2.java
@@ -10,6 +10,11 @@ public class PO2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // making two threats one is supervisor all, second the rest
+        GameThreat Game = new GameThreat();
+        SupervisorThreat Supervisor = new SupervisorThreat();
         
+        Game.run();
+        Supervisor.run();
     }
 }
