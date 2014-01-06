abf25229374a3273e561087de83e03f36c399e63
Artur <Artur@192.168.1.15>
2014-01-06 17:10:31 +0100

    Początek.

---
+++ src/po/ver/pkg2/POVer2.java
@@ -0,0 +1,22 @@
+/*
+    Z braku czasu, tym razem piszemy po polsku.
+
+    W okienku wyświetla się pole z tekstem, który trzeba przepisać.
+    W polu niżej wprowadzasz swój tekst, gdy klikniesz przycisk stop.
+    Wyświetli się wtedy okienko z wynikiem i rankingiem.
+    Koniec.
+
+*/
+package po.ver.pkg2;
+/**
+ *
+ * @author Artur
+ */
+public class POVer2 {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
